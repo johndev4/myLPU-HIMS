9a31af723783fa0b5c799f8f7de76ea27e56c4e5
LMAO <lmao@proton.com>
2021-09-28 20:06:24 +0800

    commit otday

--- lycean/app/Views/components/dashboard.php
+++ lycean/app/Views/components/dashboard.php
@@ -1,16 +1,74 @@
 <?= $this->extend('layouts/main') ?>
 <?= $this->section('content') ?>
 
-<!-- CONTENT HERE -->
-<!-- CONTENT HERE -->
-<!-- CONTENT HERE -->
+
+<body>
+    <div class="container">
+
+        <div class="mt-5 mb-5">
+            <h1 class="d-inline">Good Day </h1>
+            <span class="d-inline font-weight-bold" style="font-size: 30pt;"> Vince</span>
+            <h2 class="d-inline" style="font-size: 30pt;">!</h2>
+        </div>
+
+        <br>
+        <div class="row">
+            <div class="col-md-6 d-flex justify-content-center" style="border:1px solid none">
+                <div class="card shadow">
+
+                    <div class="row mt-5" style="border:1px solid none">
+
+                        <div class="col-7 pt-3 mt-n2 pl-4" style="border:1px solid none">
+                            <span class="font-weight-bold" style="font-size: 25pt;">Consult</span>
+                        </div>
+
+                        <div class="col-5 mt-n2" align="center" style="border:1px solid none">
+                            <i class="fas fa-comment-medical fa-5x"></i>
+                        </div>
+
+                    </div>
+
+                    <div class="card-body">
+                        <p class="card-text">Feeling unwell? schedule a consultation to one of our doctor, and wait for the schedule to be sent to you. </p>
+                        <a href="#" class="btn float-right"><i class="fas fa-chevron-circle-right fa-2x"></i></a>
+                    </div>
+
+                </div>
+            </div>
 
 
+            <div class="col-md-6 d-flex justify-content-center" style="border:1px solid none">
+                <div class="card shadow">
+
+                    <div class="row mt-5" style="border:1px solid none">
+
+                        <div class="col-7 pt-3 mt-n2 pl-4" style="border:1px solid none">
+                            <span class="font-weight-bold" style="font-size: 25pt;">Mental Wellness</span>
+                        </div>
+
+                        <div class="col-5 mt-n2" align="center" style="border:1px solid none">
+                            <i class="fas fa-brain fa-5x"></i>
+                        </div>
+
+                    </div>
+
+                    <div class="card-body">
+                        <p class="card-text">
+                            Need someone to talk with? schedule a consultation to one of our guidance counselor,
+                            and wait for the schedule to be sent to you.
+                        </p>
+                        <a href="#" class="btn float-right"><i class="fas fa-chevron-circle-right fa-2x"></i></a>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+        <br><br><br><br><br><br><br>
 
 
-
-<a href="<?= base_url('auth/logout') ?>">
-    <h2>Logout</h2>
-</a>
+    </div>
+</body>
 
 <?= $this->endSection('content') ?>
--- lycean/app/Views/layouts/top_menubar.php
+++ lycean/app/Views/layouts/top_menubar.php
@@ -1,44 +1,75 @@
 <!-- Navbar -->
-<nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-        </li>
-    </ul>
+<nav class="main-header navbar navbar-expand-md navbar-light navbar-white py-3" style="background-color: #a62d38;">
+    <div class="container">
+        <div class="navbar-brand">
+            <img src="<?= base_url('assets/images/sidebar_icon.png') ?>" alt="myLpu Clinic Logo" class="brand-image" style="opacity: .8">
+            <span class="brand-text font-weight-bold text-light">myLPU Clinic</span>
+        </div>
 
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
-        <!-- Notifications Dropdown Menu -->
-        <li class="nav-item dropdown">
-            <a class="nav-link" data-toggle="dropdown" href="#">
-                <i class="far fa-bell"></i>
-                <span class="badge badge-warning navbar-badge bg-danger">15</span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <span class="dropdown-item dropdown-header">11 Notifications</span>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item">
-                    <i class="fas fa-users mr-2"></i> 8 Consultation requests
-                    <span class="float-right text-muted text-sm">12 hours</span>
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item">
-                    <i class="fas fa-file mr-2"></i> 3 new reports
-                    <span class="float-right text-muted text-sm">2 days</span>
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item dropdown-footer">Clear Notifications</a>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-                <i class="fas fa-expand-arrows-alt"></i>
-            </a>
-        </li>
-    </ul>
+        <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse order-3" id="navbarCollapse">
+
+            <!-- Right navbar links navbar-no-expand -->
+            <ul class="order-1 order-md-3 navbar-nav ml-auto">
+                <!-- Dropdown Modal -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link" data-toggle="dropdown" href="#">
+                        <i class="fas fa-user-circle fa-lg text-light mt-n1"></i>
+                        <span class="text-white ml-1 xz">Vince</span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">
+                        <a href="<?= base_url('') ?>" class="dropdown-item">
+                            <i class="fas fa-user-cog mr-2"></i> Profile
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a href="<?= base_url('auth/logout') ?>" class="dropdown-item">
+                            <i class="fas fa-sign-out-alt mr-2"></i> Logout
+                        </a>
+                    </div>
+                </li>
+
+                <!-- Usernmame -->
+                <!-- <li>
+                    <div class="nav-link ml-n4" data-toggle="dropdown" href="#">
+                      
+                    </div>
+                </li> -->
+                <!-- /Usernmame -->
+
+                <!-- Notifications Dropdown Menu -->
+                <li class="nav-item dropdown dpst">
+                    <a class="nav-link" data-toggle="dropdown" href="#">
+                        <i class="far fa-bell fa-lg text-light"></i>
+                        <span class="text-white ml-1 notification">Notification</span>
+                        <!-- Notification Badge-->
+                        <span class="badge badge-danger navbar-badge">15</span>
+                        <!-- /Notification Badge-->
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                        <span class="dropdown-header">12 Notifications</span>
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item">
+                            <i class="fas fa-comment-medical mr-2"></i> 4 Mental wellness request
+                            <span class="float-right text-muted text-sm">3 mins</span>
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item">
+                            <i class="fas fa-stethoscope mr-2"></i> 8 Consultation requests
+                            <span class="float-right text-muted text-sm">12 hours</span>
+                        </a>
+                    </div>
+                </li>
+            </ul>
+
+        </div>
+    </div>
 </nav>
 <!-- /.navbar -->
+
+
 
 <script>
     $(document).ready(function() {
