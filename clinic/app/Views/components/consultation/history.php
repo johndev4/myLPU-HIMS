08bad8a95e1da918b698615b25feb37f40c0d853
LMAO <lmao@proton.com>
2021-10-26 20:41:47 +0800

    History Report

--- clinic/app/Views/components/consultation/history.php
+++ clinic/app/Views/components/consultation/history.php
@@ -1,6 +1,211 @@
 <?= $this->extend('layouts/main') ?>
 <?= $this->section('content') ?>
 
-<h1>His Story</h1>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-md-12">
+                    <h1 class="m-0 font-weight-normal text-black-50 d-inline">Consultation History |</h1>
+                    <span class="d-inline"><a href="<?= base_url('consultations') ?>">Back</a></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+
+            <div class="row mb-5">
+                <!-- Clear History-->
+                <div class="col-md-3 col-12 mb-3">
+                    <button type="button" class="btn btn-block btn-default shadow p-3" data-toggle="modal" data-target="#clearModal">
+                        <span class="info-box-icon add-record text-black-50"><i class="fas fa-trash"></i></span>
+                        <span class="info-box-text text-black-50">Clear History</span>
+                    </button>
+                </div>
+            </div>
+
+            <!-- Modal -->
+            <!-- Clear Modal -->
+            <div class="modal fade" id="clearModal" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content" style="height: 300px;">
+                        <div class="modal-header font-weight-bold text-secondary">
+                            Consultation History
+                        </div>
+
+                        <div class="modal-body">
+
+                            <form action="" method="get" id="add_form">
+                                <input type="hidden" name="role" value="">
+                                <div class="row">
+                                    <div class="col-12 form-group mb-n1">
+                                        <span>Time range</span>
+                                    </div>
+                                    <div class="col-6 form-group">
+                                        <label for="" class="col-form-label">From</label>
+                                        <input type="date" class="form-control" id="" name="" value="">
+                                    </div>
+                                    <div class="col-6 form-group">
+                                        <label for="" class="col-form-label">To</label>
+                                        <input type="date" class="form-control" id="" name="" value="">
+                                    </div>
+                                    <div class="col-12 form-group mt-2">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                            <label class="form-check-label" for="flexCheckDefault">
+                                                Clear all consultation history
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="float-right mt-2">
+                                    <button type="button" class="btn" data-dismiss="modal">Cancel</button>
+                                    <button type="submit" class="btn btn-danger swalDefaultSuccess ">Clear Data</button>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /Clear Modal -->
+            <!-- /Modal -->
+
+            <!-- Tabs -->
+            <div class="col-sm-12 col-md-12">
+                <div class="card card-danger card-outline card-outline-tabs">
+                    <div class="card-header p-0 border-bottom-0">
+                        <ul class="nav nav-tabs" id="custom-tabs-four-tab" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="custom-tabs-four-accepted-tab" data-toggle="pill" href="#custom-tabs-four-accepted" role="tab" aria-controls="custom-tabs-four-accepted" aria-selected="true">Accepted</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="custom-tabs-four-rejected-tab" data-toggle="pill" href="#custom-tabs-four-rejected" role="tab" aria-controls="custom-tabs-four-rejected" aria-selected="false">Rejected</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="card-body" style=" overflow-y:auto">
+
+                        <div class="tab-content" id="custom-tabs-four-tabContent">
+                            <!-- Accepted Table -->
+                            <div class="tab-pane fade show active" id="custom-tabs-four-accepted" role="tabpanel" aria-labelledby="custom-tabs-four-accepted-tab">
+
+                                <!-- Table -->
+                                <table id="acceptedTable" class="table table-bordered table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>ID Number</th>
+                                            <th>Student Name</th>
+                                            <th>Department</th>
+                                            <th>Consultation Date</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>2019-2-69266</td>
+                                            <td>John Rafael Mistica</td>
+                                            <td>COECSA</td>
+                                            <td>10-26-2021</td>
+                                            <td align="center">
+                                                <button type="button" class="btn btn-default" data-toggle="modal" data-target="#modifyModal">view</button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>2019-2-0236</td>
+                                            <td>Jade Anne Kristel Vale</td>
+                                            <td>COECSA</td>
+                                            <td>10-26-2021</td>
+                                            <td align="center">
+                                                <button type="button" class="btn btn-default" data-toggle="modal" data-target="#modifyModal">view</button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <!-- /Table -->
+
+                            </div>
+                            <!-- /Accepted Table -->
+
+                            <!-- Rejected Table -->
+                            <div class="tab-pane fade" id="custom-tabs-four-rejected" role="tabpanel" aria-labelledby="custom-tabs-four-rejected-tab">
+
+                                <!-- Table -->
+                                <table id="rejectedTable" class="table table-bordered table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>ID Number</th>
+                                            <th>Student Name</th>
+                                            <th>Department</th>
+                                            <th>Consultation Date</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>2019-2-69266</td>
+                                            <td>John Rafael Mistica</td>
+                                            <td>COECSA</td>
+                                            <td>10-26-2021</td>
+                                            <td align="center">
+                                                <button type="button" class="btn btn-default" data-toggle="modal" data-target="#modifyModal">view</button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <!-- /Table -->
+
+                            </div>
+                            <!-- /Rejected Table -->
+                        </div>
+                    </div>
+                    <!-- /.card -->
+                </div>
+            </div>
+            <!-- /Tabs -->
+
+
+
+
+
+
+
+
+
+        </div>
+    </section>
+</div>
+<!-- /.container-fluid -->
+
+<script>
+    $(document).ready(function() {
+        // For datatable
+        $("#acceptedTable").DataTable({
+            responsive: true,
+            lengthChange: true,
+            autoWidth: true,
+            processing: true,
+            searching: false,
+        });
+
+        $("#rejectedTable").DataTable({
+            responsive: true,
+            lengthChange: true,
+            autoWidth: true,
+            processing: true,
+            searching: false,
+        });
+
+        // For sidebar
+        $("#consultationNav > a").addClass('active');
+
+    });
+</script>
+<!-- /Script -->
 
 <?= $this->endSection('content') ?>
--- clinic/app/Views/components/consultation/report.php
+++ clinic/app/Views/components/consultation/report.php
@@ -1,6 +1,106 @@
 <?= $this->extend('layouts/main') ?>
 <?= $this->section('content') ?>
 
-<h1>Repo RT</h1>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 font-weight-normal text-black-50 d-inline">Report |</h1>
+                    <span class="d-inline"><a href="<?= base_url('consultations') ?>">Back</a></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+
+            <!-- Modal -->
+            <!-- Delete Modal -->
+            <div class="modal fade" id="deleteModal" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document" style="width:350px;">
+                    <div class="modal-content">
+                        <div class="modal-body">
+                            <div class="text-center mt-2">
+                                <span class="info-box-icon text-danger"><i class="fas fa-3x fa-exclamation-circle"></i></span>
+                                <div class="mt-3 font-weight-bold" style="font-size: 14pt;">Are you sure?</div>
+                                <div class="mt-1 font-weight-normal text-secondary">This will permanently remove the medicine from the inventory</div>
+                            </div><br>
+                            <div class="float-right">
+                                <form action="" method="get" id="deleteModalForm">
+                                    <button type="button" class="btn" data-dismiss="modal">Cancel</button>
+                                    <button type="submit" class="btn btn-danger swalDefaultSuccess ">Delete</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /Delete Modal -->
+            <!-- /Modal -->
+
+            <!-- Table -->
+            <div class="card">
+                <div class="card-header inventory-header">
+                    <h3 class="card-title">Report</h3>
+                </div>
+
+                <div class="card-body">
+                    <table id="example1" class="table table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th>ID Number</th>
+                                <th>Student Name</th>
+                                <th>Department</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>2019-2-69266</td>
+                                <td>John Rafael Mistica</td>
+                                <td>COECSA</td>
+                                <td>10-25-2021</td>
+                            </tr>
+                            <tr>
+                                <td>2019-2-0236</td>
+                                <td>Jade Anne Kristel Vale</td>
+                                <td>COECSA</td>
+                                <td>10-25-2021</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+            <!-- /Table -->
+
+        </div>
+    </section>
+</div>
+<!-- /.container-fluid -->
+
+<script>
+    $(document).ready(function() {
+        // For datatable
+        $("#example1").DataTable({
+            responsive: true,
+            lengthChange: true,
+            autoWidth: true,
+            processing: true,
+            searching: true,
+            buttons: ["excel", "pdf", "print"]
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+
+        // For sidebar
+        $("#consultationNav > a").addClass('active');
+
+    });
+</script>
+<!-- /Script -->
 
 <?= $this->endSection('content') ?>
