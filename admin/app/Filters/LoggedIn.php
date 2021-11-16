dba33fc19add59b76f2b23c522687948c0e4d417
johndev4 <johndev4@gmail.com>
2021-11-17 01:31:27 +0800

    update

--- admin/app/Filters/LoggedIn.php
+++ admin/app/Filters/LoggedIn.php
@@ -30,5 +30,11 @@ class LoggedIn implements FilterInterface
                 return redirect()->to('changepassword');
             }
         }
+        if ($user != [] && $user['locked'] >= 2) {
+            // Delete login session on client
+            session()->destroy();
+            // Redirect to login page
+            return redirect()->to('login');
+        }
     }
 }
--- lycean/app/Filters/LoggedIn.php
+++ lycean/app/Filters/LoggedIn.php
@@ -32,5 +32,11 @@ class LoggedIn implements FilterInterface
                 return redirect()->to('changepassword');
             }
         }
+        if ($user != [] && $user['locked'] >= 2) {
+            // Delete login session on client
+            session()->destroy();
+            // Redirect to login page
+            return redirect()->to('login');
+        }
     }
 }
